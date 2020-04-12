28f5363f99c309b3145924997b06c3f110a5d912
yangpan12 <738912168@qq.com>
2020-04-12 16:09:02 +0800

    feat:埋点增加脑图

--- src/views/monitor.vue
+++ src/views/monitor.vue
@@ -5,6 +5,7 @@
       <a href="https://github.com/unclepan/zui-monitor" target="_blank">zui-monitor</a>
     </x-aside>
     <p>可监控三类信息：行为监控，性能监控，页面错误监控。按信息分类分成五类上报的事件类型，行为监控包含：点击事件（click），页面进入事件（stay），页面关闭事件（close）。性能监控包含：监控事件（per）。页面错误包含：错误（error）。</p>
+    <img :class="$style.pic" src="@/assets/images/monitor.png"/>
   </div>
 </template>
 
@@ -23,5 +24,9 @@ export default {
 .monitor{
   background: #ffffff;
   padding: 16px;
+  .pic{
+    width: 100%;
+    padding-top: 10px;
+  }
 }
 </style>
