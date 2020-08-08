4200c543fa967774864b9b03ddfbe6cd1471e94f
yangpan12 <738912168@qq.com>
2020-08-08 09:59:37 +0800

    feat:新增表单通用组件

--- src/components/dynamic-form-fields/index.js
+++ src/components/dynamic-form-fields/index.js
@@ -6,6 +6,9 @@ import xNumber from './number.vue';
 import Switch from './switch.vue';
 import xDate from './date.vue';
 import xUpload from './upload.vue';
+import xRadio from './radio.vue';
+import xCheckbox from './checkbox.vue';
+import xStatic from './static.vue';
 
-export const some = [Input, Select, xNumber, Switch, xDate, xUpload];
+export const some = [Input, Select, xNumber, Switch, xDate, xUpload, xRadio, xCheckbox, xStatic];
 export default new Fields(some);
---
+++ src/components/dynamic-form-fields/static.vue
@@ -0,0 +1,22 @@
+<template>
+  <container :param="param">
+    <span :class="$style.static">{{value || '字段无值'}}</span>
+  </container>
+</template>
+
+<script>
+import fieldMixin from './basic/field-mixin';
+
+export default {
+  INDEX: ['STATIC'],
+  mixins: [
+    fieldMixin,
+  ],
+};
+</script>
+<style lang="scss" module>
+.static{
+  padding-right: 12px;
+  color: #9e9e9e;
+}
+</style>
